66b65fb5642c59a081d15dc7ca221a784898ea52
Henrik Stene <henrik.stene@knowit.no>
2018-01-18 16:18:30 +0100

    Added simple test

--- src/test/java/com/example/pact/consumer/ProviderPactTests.java
+++ src/test/java/com/example/pact/consumer/ProviderPactTests.java
@@ -1,16 +1,26 @@
 package com.example.pact.consumer;
 
+import au.com.dius.pact.consumer.ConsumerPactBuilder;
+import au.com.dius.pact.consumer.MockProvider;
+import au.com.dius.pact.consumer.PactVerificationResult;
+import au.com.dius.pact.consumer.dsl.PactDslJsonBody;
+import au.com.dius.pact.model.MockProviderConfig;
+import au.com.dius.pact.model.RequestResponsePact;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import static au.com.dius.pact.consumer.ConsumerPactRunnerKt.runConsumerTest;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 
 @RunWith(SpringRunner.class)
@@ -23,5 +33,39 @@ public class ProviderPactTests {
         headers.put("Content-Type", MediaType.APPLICATION_JSON_UTF8_VALUE);
 
         ObjectMapper objectMapper = new ObjectMapper();
+
+        Person person = Person.builder().name("Roger Antonsen").ssn("71039012345").build();
+
+        RequestResponsePact pact = ConsumerPactBuilder
+                .consumer("pact-consumer").hasPactWith("pact-provider")
+                .uponReceiving("Create new person request")
+                    .path("/person")
+                    .method("POST")
+                    .headers(headers)
+                    .body(objectMapper.writeValueAsString(person))
+                .willRespondWith()
+                    .headers(headers)
+                    .status(HttpStatus.CREATED.value())
+                    .body(new PactDslJsonBody()
+                            .stringValue("name", "Roger Antonsen")
+                            .stringValue("ssn", "71039012345")
+                            .integerType("id", 0)
+                    )
+                .toPact();
+
+        MockProviderConfig config = MockProviderConfig.createDefault();
+
+        PactVerificationResult result = runConsumerTest(pact, config, mockServer -> {
+            ProviderClient providerClient = new ProviderClient(new ProviderConfig(mockServer.getUrl()));
+
+            Person personResponse = providerClient.createPerson(person);
+
+            assertEquals(personResponse.getName(), person.getName());
+            assertEquals(personResponse.getSsn(), person.getSsn());
+            assertTrue(personResponse.getId() != null);
+
+        });
+
+        assertEquals(PactVerificationResult.Ok.INSTANCE, result);
     }
 }
